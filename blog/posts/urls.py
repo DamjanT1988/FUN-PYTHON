dcec77800f936a5c4ed76d8d27a8c80c49da0fcd
DamjanT1988 <Damjan_t@hotmail.com>
2024-10-10 11:45:38 +0200

    4

---
+++ blog/posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PostListCreateView.as_view(), name='post-list-create'),  # Handle the /api/posts/ endpoint
+    path('<int:id>/', views.PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),  # Handle specific post actions
+]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Welcome to the Blog API</h1><p>Go to <a href='/api/posts/'>Posts</a></p>")
+
+# List all posts and create a new post
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# Retrieve, update, or delete a post by ID
+class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
