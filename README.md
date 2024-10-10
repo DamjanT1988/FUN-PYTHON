060a1b05e292d2b784d5c31d2663be0eab803a29
DamjanT1988 <Damjan_t@hotmail.com>
2024-10-10 12:21:36 +0200

    5

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Blog Application
+
+This is a simple blog application built with Django and Django REST Framework. It allows users to create, update, and delete posts through an API. The Django admin interface is also available to manage posts.
+
+## How to Run the Project
+
+1. Clone the repository.
+2. Install the required dependencies:
+   ```bash
+   pip install -r requirements.txt
--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListCreateView.as_view(), name='post-list-create'),  # Handle the /api/posts/ endpoint
-    path('<int:id>/', views.PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),  # Handle specific post actions
+    path('<int:pk>/', views.PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),  # Handle specific post actions
 ]
